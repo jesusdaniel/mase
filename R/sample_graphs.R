3d46c469e003a8aa48e3d49159f6ec36a7f45a73
Jesús Arroyo <jesus.daniel.a.r@gmail.com>
2019-06-24 18:46:58 -0400

    Add files via upload

--- R/sample_graphs.R
+++ R/sample_graphs.R
@@ -1,3 +1,10 @@
+#' 
+#' Function to sample an independent-edge graph given a probability matrix P
+#' 
+#' @param P matrix encoding the probabilities of an edge in the graph
+#' 
+#' @return The adjacency matrix of a random graph
+#' 
 sample_from_P <- function(P) {
   n = ncol(P)
   A = Matrix(0, n, n)
